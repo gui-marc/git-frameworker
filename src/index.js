a3dc6eb756d0ff7e00bc82ec44e1f2a91d98a2fd
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2023-05-01 22:10:17 +0100

    Custom cli

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //!/usr/bin/env node
 
 import chalk from 'chalk';
+import chalkAnimations from 'chalk-animation';
 import inquirer from 'inquirer';
 import { exec } from 'child_process';
 import { createSpinner } from 'nanospinner';
@@ -8,6 +9,7 @@ import fs from 'fs';
 import { promisify } from 'util';
 import figlet from 'figlet';
 import gradient from 'gradient-string';
+import { readdir } from 'fs/promises';
 
 const execAsync = promisify(exec);
 const rmAsync = promisify(fs.rm);
@@ -35,17 +37,31 @@ const clean = async (removeProject = false) => {
   spinner.success({ text: 'Cleaned up successfully' });
 };
 
-const greetings = async () => {
+const greetings = async (
+  name = 'Git Frameworker',
+  description = 'Easy way to create a project from a git repo\n'
+) => {
   console.clear();
-  figlet('Git Frameworker', (err, data) => {
+  figlet(name, (err, data) => {
     console.log(gradient.cristal.multiline(data));
   });
 
   await sleep(300);
 
-  console.log(chalk.cyan('  Easy way to create a project from a git repo\n'));
+  console.log(chalk.cyan(`  ${description}`));
 
   await sleep(300);
+};
+
+const finalMessage = () => {
+  chalkAnimations
+    .rainbow(`\nProject '${projectName}' created successfully\n`)
+    .start();
+
+  console.log(
+    `To start working on your project run:
+      ${chalk.cyan(`cd ${projectName}`)}\n`
+  );
 };
 
 const getRepo = async () => {
@@ -96,7 +112,6 @@ const createProject = async () => {
 
   try {
     await mkdirAsync(projectName);
-    await cpAsync('.git-repo-temp', projectName, { recursive: true });
   } catch (err) {
     spinner.error({ text: `Error creating project\n${err}` });
     process.exit(1);
@@ -107,7 +122,140 @@ const createProject = async () => {
   });
 };
 
-await greetings();
-await getRepo();
-await createProject();
-await clean(process.argv[2] === '--clean');
+const repoHasFrameworkConfig = async () => {
+  const spinner = createSpinner(
+    'Checking if repo has framework config...'
+  ).start();
+
+  try {
+    const files = await readdir('.git-repo-temp');
+    const result = files.includes('framework.json');
+
+    if (result) {
+      spinner.success({ text: 'Found framework config' });
+    } else {
+      spinner.warn({ text: 'Repo does not have framework config' });
+    }
+
+    return result;
+  } catch (e) {
+    spinner.error({
+      text: `Error checking if repo has framework config\n${e}`
+    });
+    process.exit(1);
+  }
+};
+
+const parseConfig = async () => {
+  const spinner = createSpinner('Parsing framework config...').start();
+
+  try {
+    await sleep(300);
+
+    const configContent = await fs.promises.readFile(
+      '.git-repo-temp/framework.json',
+      'utf-8'
+    );
+
+    const config = JSON.parse(configContent);
+    spinner.success({ text: 'Parsed framework config' });
+    return config;
+  } catch (e) {
+    spinner.error({ text: `Error parsing framework config\n${e}` });
+    process.exit(1);
+  }
+};
+
+const inquiryCli = async (config) => {
+  const { frameworkName, frameworkDescription, questions } = config;
+
+  console.clear();
+  await greetings(frameworkName, frameworkDescription);
+  console.log('');
+
+  let answers = [];
+
+  for (let question of questions) {
+    const { name, type, message, choices, isPartial } = question;
+
+    const answer = await inquirer.prompt({
+      name,
+      type,
+      message,
+      choices
+    });
+
+    answers.push({ name, answer: answer[name], type, isPartial });
+  }
+
+  const partials = answers.filter((answer) => answer.isPartial);
+  const fulls = answers.filter((answer) => !answer.isPartial);
+
+  const templateDir = fulls
+    .filter((f) => f.type !== 'input')
+    .map((f) => f.name + ':' + f.answer.toString())
+    .join('_')
+    .toLowerCase();
+
+  return { partials, templateDir };
+};
+
+const buildProject = async (config) => {
+  const { partials, templateDir } = await inquiryCli(config);
+
+  const spinner = createSpinner('Building project...').start();
+  try {
+    await cpAsync('.git-repo-temp/templates/' + templateDir, projectName, {
+      recursive: true
+    });
+
+    for (let partial of partials) {
+      if (partial.type === 'list' || partial.type === 'confirm') {
+        if (partial.type === 'confirm' && partial.answer) {
+          await cpAsync(
+            '.git-repo-temp/partials/' + partial.name,
+            projectName,
+            {
+              recursive: true
+            }
+          );
+        } else if (partial.type === 'list') {
+          await cpAsync(
+            '.git-repo-temp/partials/' +
+              partial.name +
+              ':' +
+              partial.answer.toString().toLowerCase(),
+            projectName,
+            {
+              recursive: true
+            }
+          );
+        }
+      }
+    }
+
+    spinner.success({ text: 'Project built successfully' });
+  } catch (e) {
+    spinner.error({ text: `Error building project\n${e}` });
+    process.exit(1);
+  }
+};
+
+async function main() {
+  await greetings();
+  await getRepo();
+  await createProject();
+
+  let hasFrameworkConfig = await repoHasFrameworkConfig();
+  if (hasFrameworkConfig) {
+    const config = await parseConfig();
+    await buildProject(config);
+  } else {
+    await cpAsync('.git-repo-temp', projectName, { recursive: true });
+  }
+
+  await clean(process.argv[2] === '--clean');
+  finalMessage();
+}
+
+await main();
